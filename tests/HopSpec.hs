694b306665083429a0f53ef10433dd579501f254
Taylor Fausak <taylor@fausak.me>
2014-02-08 09:48:18 -0600

    Add an example QuickCheck spec

--- tests/HopSpec.hs
+++ tests/HopSpec.hs
@@ -1,12 +1,14 @@
 module HopSpec (main, spec) where
 
-import           Hop
-import           Test.Hspec
+import           Hop             (hop)
+import           Test.Hspec      (Spec, describe, hspec, it, shouldBe)
+import           Test.QuickCheck (property)
 
 main :: IO ()
 main = hspec spec
 
 {-# ANN spec "HLint: ignore Redundant do" #-}
+
 spec :: Spec
 spec = do
   describe "hop" $ do
@@ -15,3 +17,6 @@ spec = do
 
     it "returns the right number of hops" $ do
       hop 3 `shouldBe` "Hop! Hop! Hop!"
+
+    it "is null for integers less than one" . property $
+      \ n -> if n < 1 then null (hop n) else not (null (hop n))
