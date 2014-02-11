413568bddf63661bfc2d0c3c6a4f5b284db6f371
Taylor Fausak <taylor@fausak.me>
2014-02-10 23:00:09 -0600

    Use `prop` instead of `it "..." . property`

--- tests/HopSpec.hs
+++ tests/HopSpec.hs
@@ -1,9 +1,10 @@
 module HopSpec (main, spec) where
 
-import           Hop             (hop)
-import           Test.Hspec      (Spec, describe, hspec, it, shouldBe)
-import           Test.HUnit      ()
-import           Test.QuickCheck (property)
+import           Hop                   (hop)
+import           Test.Hspec            (Spec, describe, hspec, it, shouldBe)
+import           Test.Hspec.QuickCheck (prop)
+import           Test.HUnit            ()
+import           Test.QuickCheck       ()
 
 main :: IO ()
 main = hspec spec
@@ -17,5 +18,5 @@ spec = do
         it "returns the right number of hops" $ do
             hop 3 `shouldBe` "Hop! Hop! Hop!"
 
-        it "is null for integers less than one" . property $
+        prop "is null for integers less than one" $
             \ n -> if n < 1 then null (hop n) else not (null (hop n))
