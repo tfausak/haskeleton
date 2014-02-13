651198c80f4d0f45052d2ce0adaafaa8e0482afa
Taylor Fausak <taylor@fausak.me>
2014-02-12 23:13:32 -0600

    Fix #13; automate documentation

--- tests/Haddock.hs
+++ tests/Haddock.hs
@@ -1,4 +1,24 @@
 module Main (main) where
 
+import           Data.List      (genericLength)
+import           Data.Maybe     (catMaybes)
+import           System.Exit    (exitFailure, exitSuccess)
+import           System.Process (readProcess)
+import           Text.Regex     (matchRegex, mkRegex)
+
+average :: (Fractional a, Real b) => [b] -> a
+average xs = realToFrac (sum xs) / genericLength xs
+
+expected :: Fractional a => a
+expected = 90
+
 main :: IO ()
-main = return ()
+main = do
+    output <- readProcess "cabal" ["haddock"] ""
+    let actual = average (match output)
+    if actual >= expected then exitSuccess else exitFailure
+
+match :: String -> [Int]
+match = fmap read . concat . catMaybes . fmap (matchRegex pattern) . lines
+  where
+    pattern = mkRegex "^ ([0-9]*)% "
